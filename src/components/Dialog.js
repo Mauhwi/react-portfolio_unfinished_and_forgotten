232873ff6955fe57922c5c8a730783c16f994a1d
Mauhwi <tykityki7@gmail.com>
2021-02-24 04:56:36 +0500

    renders twice so idk

--- src/components/Dialog.js
+++ src/components/Dialog.js
@@ -2,15 +2,41 @@ import React, { Component } from "react";
 
 export class Dialog extends Component {
 
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 0,
+      isLighten: false,
+      storyMode: false,
+      currentText: 1,
+      dialogText: textData
+    };
+  }
+
   render() {
+
     let avatar;
     let text;
+    let stringArray =[];
+
+    function stringAnimation() {
+      if (stringArray.length > 0) {
+        document.querySelector('.text').innerHTML += stringArray.shift();
+      } else {
+        clearTimeout();
+      }
+      setTimeout(stringAnimation, 50);
+    }
+
     if (!this.props.isLighten) {
       avatar = <img src="avatar-dark.png" alt="background"></img>;
       text = "...";
     } else {
         avatar = <img src="avatar-dark-bg.png" alt="background"></img>;
-        text = this.props.text.filter(textLine => textLine.id == this.props.textId).map(textLine => textLine.engText);
+        text = this.props.text.filter(textLine => textLine.id == this.props.textId).map(textLine => textLine.engText).toString();
+        stringArray = text.split("");
+        console.log(text)
+        stringAnimation();
     }
 
 
@@ -20,7 +46,7 @@ export class Dialog extends Component {
           <div className="image-wrap">
             {avatar}
           </div>
-          <div className="text">{text}</div>
+          <div className="text"></div>
         </div>
       </div>
     );
