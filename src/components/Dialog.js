0b98041224bc865a6582e4fd6e90d09a8edc0da4
Mauhwi <tykityki7@gmail.com>
2021-02-24 15:21:07 +0500

    apparetly it only renders twice in dev build so we are probably ok (probably)

--- src/components/Dialog.js
+++ src/components/Dialog.js
@@ -1,51 +1,44 @@
 import React, { Component } from "react";
 
 export class Dialog extends Component {
-
   constructor(props) {
     super(props);
     this.state = {
-      count: 0,
-      isLighten: false,
-      storyMode: false,
-      currentText: 1,
-      dialogText: textData
+      stringArray: [],
     };
   }
 
-  render() {
+  stringAnimation() {
+    document.querySelector(".text").innerHTML = "";
+    this.state.stringArray.forEach((element, index) => {
+      setTimeout(function name(params) {
+        document.querySelector(".text").innerHTML += element;
+      }, 50 * (index + 1));
+    });
+  }
 
+  render() {
     let avatar;
     let text;
-    let stringArray =[];
-
-    function stringAnimation() {
-      if (stringArray.length > 0) {
-        document.querySelector('.text').innerHTML += stringArray.shift();
-      } else {
-        clearTimeout();
-      }
-      setTimeout(stringAnimation, 50);
-    }
+    let stringArray = [];
 
     if (!this.props.isLighten) {
       avatar = <img src="avatar-dark.png" alt="background"></img>;
       text = "...";
     } else {
-        avatar = <img src="avatar-dark-bg.png" alt="background"></img>;
-        text = this.props.text.filter(textLine => textLine.id == this.props.textId).map(textLine => textLine.engText).toString();
-        stringArray = text.split("");
-        console.log(text)
-        stringAnimation();
+      avatar = <img src="avatar-dark-bg.png" alt="background"></img>;
+      text = this.props.text
+        .filter((textLine) => textLine.id == this.props.textId)
+        .map((textLine) => textLine.engText)
+        .toString();
+      this.state.stringArray = text.split("");
+      this.stringAnimation();
     }
-
 
     return (
       <div className="Dialog">
         <div className="content-wrap">
-          <div className="image-wrap">
-            {avatar}
-          </div>
+          <div className="image-wrap">{avatar}</div>
           <div className="text"></div>
         </div>
       </div>
